12a9ef77266d0dd2b276b663f74d15a49a429093
gondaoliver <gondaoliver@pm.me>
2026-01-04 12:56:41 +0100

    Made a camera script with CV2

---
+++ camera.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret, frame = cap.read()
+ 
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('SufniCam', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+cap.release()
+cv.destroyAllWindows()

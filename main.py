19a5a3004913c709ca1fbc7d8870d154224b806f
Gonda Olivér <gondaoliver@pm.me>
2026-06-26 12:53:57 +0200

    Changed up fw and bw

--- main.py
+++ main.py
@@ -446,14 +446,14 @@ class MainWindow(QMainWindow):
                 while speed < self.preset_speed:
                     speed += 0.1
                 speed = min(speed, self.preset_speed)
-                bw(speed) if inverted else fw(speed)
+                fw(speed) if inverted else bw(speed)
                 print(f"Forward: {speed}")
 
             elif event.key() == Qt.Key_S:
                 while speed < self.preset_speed:
                     speed += 0.1
                 speed = min(speed, self.preset_speed)
-                fw(speed) if inverted else bw(speed)
+                bw(speed) if inverted else fw(speed)
                 print(f"Backward: {speed}")
 
             elif event.key() == Qt.Key_A:
